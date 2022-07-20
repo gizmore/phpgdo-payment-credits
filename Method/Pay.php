dc92c25b6ac00f0769ff2c16390c9e5c7701266b
gizmore <gizmore@wechall.net>
2022-07-20 04:46:37 +0200

    '7.0.0-r1371'

--- Method/Pay.php
+++ Method/Pay.php
@@ -15,7 +15,7 @@ use GDO\Payment\Module_Payment;
  */
 final class Pay extends Method
 {
-	public function showInSitemap() { return false; }
+	public function showInSitemap() : bool { return false; }
 	public function isAlwaysTransactional() { return true; }
 	
 	public function execute()
