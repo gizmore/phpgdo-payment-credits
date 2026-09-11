45981530bd80deb8c034b02c9a634de13fdf422c
Mira <mira@mira-gpt.org>
2026-09-11 05:18:33 +0200

    Fix credits order card rendering

--- tpl/card/credits_order.php
+++ tpl/card/credits_order.php
@@ -1,1 +1,21 @@
-<?phpuse GDO\PaymentCredits\GDO_CreditsOrder;use GDO\UI\GDT_Card;use GDO\UI\GDT_Paragraph;/** @var $gdo GDO_CreditsOrder * */$gdo instanceof GDO_CreditsOrder;$card = GDT_Card::make();$card->title('card_title_credits_order', [$gdo->getCredits()]);$card->subtitle('card_title_credits_price', [$gdo->getCredits(), $gdo->displayPrice()]);$card->addField(	GDT_Paragraph::make()->text('card_info_credits_price', [$gdo->getCredits(), $gdo->displayPrice()]));echo $card->render();
+<?php
+
+use GDO\PaymentCredits\GDO_CreditsOrder;
+use GDO\UI\GDT_Card;
+use GDO\UI\GDT_Headline;
+use GDO\UI\GDT_Paragraph;
+use GDO\UI\GDT_Title;
+
+/** @var $gdo GDO_CreditsOrder * */
+$gdo instanceof GDO_CreditsOrder;
+
+$card = GDT_Card::make();
+
+$card->title(GDT_Title::make()->title('card_title_credits_order', [$gdo->getCredits()]));
+$card->subtitle(GDT_Headline::make()->text('card_title_credits_price', [$gdo->getCredits(), $gdo->displayPrice()])->level(5));
+
+$card->addField(
+	GDT_Paragraph::make()->text('card_info_credits_price', [$gdo->getCredits(), $gdo->displayPrice()])
+);
+
+echo $card->render();
