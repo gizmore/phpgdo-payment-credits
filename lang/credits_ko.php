b63bc6039149deefe0fc9cf1136f9009c95b4a69
gizmore <gizmore@wechall.net>
2026-09-18 04:08:42 +0200

    'more korean'

---
+++ lang/credits_ko.php
@@ -0,0 +1,30 @@
+<?php
+return [
+	'module_paymentcredits' => '크레딧',
+	'credits' => '크레딧',
+	'cfg_fee_buy' => '크레딧으로 구매 시 수수료',
+	'cfg_paycreds_guests' => '손님이 크레딧으로 구매할 수 있도록 허용',
+	'cfg_paycreds_min_purchase' => '크레딧의 최소 주문 금액',
+	'cfg_paycreds_rate' => '1학점의 가치',
+	'link_credits' => '크레딧(%s)',
+	'mt_paymentcredits_ordercredits' => '크레딧 구매',
+	'card_title_credits_order' => '%s 크레딧 구매',
+	'card_title_credits_price' => '%s에 대해 %s 크레딧을 구매하세요.',
+	'card_info_credits_price' => '크레딧 계좌를 %s만큼 늘리세요.<br/>이 비용은 %s이며 되돌릴 수 없습니다.',
+	'info_credits_exchange_rate' => '현재 환율: 1크레딧 = %s.',
+	'buy_paymentcredits' => '[%s]크레딧',
+	'btn_pay_credits' => '%s 중 %s 크레딧 지불',
+	'msg_credits_purchased' => '<b>%s</b> 크레딧을 구매했으며 잔액을 %s에서 <b>%s</b>(으)로 늘렸습니다.',
+	'err_no_credits' => '크레딧이 충분하지 않습니다.<br/>주문 총액은 %s이며 이는 %s 크레딧과 동일하며 귀하는 %s을(를) 보유하고 있습니다.',
+	'link_grant_credits' => '크레딧 부여',
+	'mt_paymentcredits_grantcredits' => '크레딧 부여',
+	'msg_credits_granted' => '%s 크레딧을 %s에 부여했습니다. 사용자는 현재 %s 크레딧을 보유하고 있습니다.',
+	'mail_subj_credits_granted' => '부여된 학점',
+	'mail_body_credits_granted' => '안녕하세요 %s님,
+
+%s에 %s 크레딧을 받았습니다.
+이제 %s 크레딧을 소유하게 되었습니다.
+
+감사합니다
+%3$s 팀',
+];
